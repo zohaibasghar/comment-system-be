fbe9c18b07263bac9e4a7bb16d1369590806248d
zohaib asghar <zohaibmehar123456@gmail.com>
2024-06-16 17:00:53 +0500

    fix

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const CommentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
---
+++ routes/comments.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const Comment = require("../models/Comment");
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
+
+router.get("/", async function (req, res) {
+  try {
+    const comments = await Comment.find();
+    return res.status(200).json(comments);
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    throw error;
+  }
+});
+
+router.post(
+  "/",
+  [
+    body("name", "Name field in required!").exists(),
+    body("email", "Enter a valid email!").isEmail(),
+    body("comment", "Comment field is requried!").exists(),
+  ],
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const comment = await Comment.create(req.body);
+      return res.status(201).json({
+        message: "Thanks for your comment, we will get back to you soon!",
+        comment,
+      });
+    } catch (error) {
+      res.send({
+        code: 401,
+        error: true,
+        message: error.message,
+      });
+    }
+  }
+);
+module.exports = router;
